5180774d7219c0168ec187a33215420f6c24b157
WU <652847772@qq.com>
2023-01-11 11:41:56 +0800

    init

---
+++ cn-product/cn-product-api/src/main/java/cn/fuling/cn/product/api/ProductApi.java
@@ -0,0 +1,14 @@
+package cn.fuling.cn.product.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author 秦
+ * @date 2023/1/11 10:11
+ * @description
+ */
+@RequestMapping("/inner")
+public interface ProductApi {
+
+
+}
