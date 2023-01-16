5dc178f693b184978d8aac41cfeba27a4fc45599
WU <652847772@qq.com>
2023-01-16 11:46:48 +0800

    init

---
+++ cn-product/cn-product-api/src/main/java/cn/fuling/cn/product/utils/UUU.java
@@ -0,0 +1,9 @@
+package cn.fuling.cn.product.utils;
+
+/**
+ * @author 秦
+ * @date 2023/1/16 11:46
+ * @description
+ */
+public class UUU {
+}
---
+++ cn-product/cn-product-api/src/main/java/cn/fuling/cn/product/vo/response/WWW.java
@@ -0,0 +1,9 @@
+package cn.fuling.cn.product.vo.response;
+
+/**
+ * @author 秦
+ * @date 2023/1/16 11:46
+ * @description
+ */
+public class WWW {
+}
