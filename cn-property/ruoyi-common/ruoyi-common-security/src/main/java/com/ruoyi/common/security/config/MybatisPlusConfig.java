87e1693dbd3c954ecaf9568d92f9846c3af53bc8
jiangshan <3155362902@qq.com>
2023-03-14 17:14:16 +0800

    修改pom依赖包名

---
+++ cn-property/ruoyi-common/ruoyi-common-security/src/main/java/com/ruoyi/common/security/config/MybatisPlusConfig.java
@@ -0,0 +1,26 @@
+package com.ruoyi.common.security.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.pagination.optimize.JsqlParserCountOptimize;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author:远山
+ * @Date: 2023/3/14  16:47
+ * @Version 1.0
+ */
+@Configuration
+public class MybatisPlusConfig {
+	/**
+	 *配置分页
+	 */
+	@Bean
+	public  MybatisPlusInterceptor mybatisPlusInterceptor() {
+		MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+		interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+		return interceptor;
+	}
+}
