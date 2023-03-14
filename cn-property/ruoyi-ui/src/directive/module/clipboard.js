700675d794150ae1b3f00e70c419e69a2c639685
WU <652847772@qq.com>
2023-03-14 15:34:59 +0800

    init

--- cn-property/ruoyi-ui/src/directive/module/clipboard.js
+++ cn-property/ruoyi-ui/src/directive/module/clipboard.js
@@ -3,7 +3,7 @@
 * Copyright (c) 2021 ruoyi
 */
 
-import Clipboard from 'clipboard'
+import Clipboard from 'ruoyi-ui/src/directive/module/clipboard'
 export default {
   bind(el, binding, vnode) {
     switch (binding.arg) {
