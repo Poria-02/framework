bee3f8cf543643534e06bba7e6e1f4712e89d578
wyf <652847772@qq.com>
2023-04-25 11:17:36 +0800

    init

---
+++ cn-property/cn-common/cn-common-core/src/main/java/cn/common/core/exception/PreAuthorizeException.java
@@ -0,0 +1,15 @@
+package cn.common.core.exception;
+
+/**
+ * 权限异常
+ * 
+ * @author ruoyi
+ */
+public class PreAuthorizeException extends RuntimeException
+{
+    private static final long serialVersionUID = 1L;
+
+    public PreAuthorizeException()
+    {
+    }
+}
